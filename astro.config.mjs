adf85ee89b85293108898926c49a784cd30b2c50
jeols <jens.moeller.olsen@systematic.com>
2025-07-28 06:42:36 +0200

    ET-0000 Fixed styling by adding values for gh repo in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://rns2025.github.io',
+  base: '/Company_Website/',
+
+  integrations: [tailwind()]
 });
