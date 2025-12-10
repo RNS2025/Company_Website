97a473ed49a742303155124aa8a876e20de00b6e
Olsen7351 <119688866+Olsen7351@users.noreply.github.com>
2025-12-10 12:00:13 +0100

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://rns2025.github.io',
-  base: '/Company_Website/',
+  site: 'https://www.rns-apps.dk', 
+  base: '/',                       
   trailingSlash: 'always',
 
   integrations: [tailwind()]
