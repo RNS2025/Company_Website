c8b388620f3dddf4f5f47ca932579dd4dc10f74b
Olsen7351 <olsen7351@gmail.com>
2025-12-12 14:13:27 +0100

    Speed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: 'https://www.rns-apps.dk',
   base: '/',
   trailingSlash: 'always',
 
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  },
+
   integrations: [
     tailwind(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    })
+    sitemap()
   ]
 });
