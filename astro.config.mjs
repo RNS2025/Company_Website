f7992bc67616e75a7f7ff2383fad27c10cae9dfa
Olsen7351 <olsen7351@gmail.com>
2025-12-13 08:36:23 +0100

    G4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  site: 'https://www.rns-apps.dk',
-  base: '/',
-  trailingSlash: 'always',
-
+  site: "https://www.rns-apps.dk",
+  base: "/",
+  trailingSlash: "always",
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    tailwind(),
+    sitemap(),
+  ],
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: 'viewport'
+    defaultStrategy: "viewport",
   },
-
-  integrations: [
-    tailwind(),
-    sitemap()
-  ]
 });
